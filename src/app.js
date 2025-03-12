d08a6d5865b9d9c76e9e66bd9d202cd7412a0584
FelipeFlores <110569081+erfeli@users.noreply.github.com>
2025-03-12 01:14:14 +0000

    corregido

--- src/app.js
+++ src/app.js
@@ -1,41 +1,30 @@
-/*import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let exten = [".com", ".es", ".br.com"];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-
-  console.log("Hello Rigo from the console!");
-};*/
-
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon']
-let exten = ['.com', '.es', '.br.com',]
-
-/*for (let i = 0; i < pronoun.length; i++) {
+for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let h = 0; h < noun.length; h++) {
       for (let f = 0; f < exten.length; f++) {
-          console.log(pronoun[i] + adj[j] + noun[h] + exten[f])
-
+        console.log(pronoun[i] + adj[j] + noun[h] + exten[f]);
+      }
     }
   }
 }
-}*/
 
 //hack
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let h = 0; h < noun.length; h++) {
-      
-          console.log(pronoun[i] + adj[j] + noun[h].slice(0,noun[h].length-2)+'.'+noun[h].slice(noun[h].length-2,noun[h].length))
-
-    
+      console.log(
+        pronoun[i] +
+          adj[j] +
+          noun[h].slice(0, noun[h].length - 2) +
+          "." +
+          noun[h].slice(noun[h].length - 2, noun[h].length)
+      );
+    }
   }
-}
 }
